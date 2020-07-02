0ba7e9e65ddca09f2786c729162aa8992ee47b72
Ybrayym <ybrayymjan90@gmail.com>
2020-07-02 11:20:26 -0400

    code has been resurrected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {Shoes:[]}
+  }
+  componentDidMount(){
+    fetch('http://127.0.0.1:8000/api/shoe/')
+    .then(res => res.json())
+    .then(res => this.setState({Shoes: res}))
+  }
+  render() {return (
+        <div>
+          <ul>{this.state.Shoes.map(s =>{
+            return(
+              <div>
+                <ul>
+                Shoe : {s.name}
+                </ul>
+                <ul>
+                  Size : {s.size}
+                </ul>
+                <ul>
+                  Manufacturer : {s.manufacturer}
+                </ul>
+                <ul>
+                  Color : {s.color}
+                </ul>
+                <ul>
+                  Material : {s.material}
+                </ul>
+                <ul>
+                  Shoe Type : {s.shoe_type}
+                </ul>
+                <ul>
+                  Fasten Type : {s.fasten_type}
+                </ul> <br></br>
+            </div>
+          )
+          })}</ul>
+        </div>
+      )
+  }
 }
 
 export default App;
